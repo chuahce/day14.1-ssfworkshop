e7e80a155f4ace5ec70276541d2fd3ce77c41e8b
chuahce <choongeng.chuah@gmail.com>
2023-02-28 10:35:57 +0800

    Initial Commit

---
+++ src/test/java/nus/iss/tfip/ssf/day14/ssfworkshop/ApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.iss.tfip.ssf.day14.ssfworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
